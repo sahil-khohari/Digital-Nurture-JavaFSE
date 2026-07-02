3fd36332c3a71bd0747813155ea722c1b5e31c27
Sahil Yadav <sahilyadav130305@gmail.com>
2026-07-03 02:05:26 +0530

    Completed Module 4 Exercise 1.4 - AAA Pattern

---
+++ Week-1/M04_JUnit_Mockito_SLF4J/Ex1.4_AAA_Pattern/src/test/java/com/cognizant/junit/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.cognizant.junit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setup: Creating Calculator object");
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Teardown: Cleaning resources");
+        calculator = null;
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+}
