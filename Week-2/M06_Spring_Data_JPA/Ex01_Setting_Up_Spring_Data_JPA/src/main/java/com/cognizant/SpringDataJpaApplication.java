236c3be1c3eaa95e998d4c3f3e33ebf138eeaeca
Sahil Yadav <sahilyadav130305@gmail.com>
2026-07-05 20:18:21 +0530

    Completed M06 Ex01 - Setting Up Spring Data JPA

---
+++ Week-2/M06_Spring_Data_JPA/Ex01_Setting_Up_Spring_Data_JPA/src/main/java/com/cognizant/SpringDataJpaApplication.java
@@ -0,0 +1,24 @@
+package com.cognizant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaApplication implements CommandLineRunner {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        repository.save(new Employee("Sahil"));
+
+        System.out.println("Employee saved successfully.");
+    }
+}
