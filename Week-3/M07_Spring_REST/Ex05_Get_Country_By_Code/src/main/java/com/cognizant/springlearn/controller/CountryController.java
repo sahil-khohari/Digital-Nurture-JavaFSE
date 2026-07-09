eba82440833c679afc4dba57b4caab27c1ecda45
Sahil Yadav <sahilyadav130305@gmail.com>
2026-07-10 01:58:11 +0530

    Completed M07 Ex05 - Get Country By Code

---
+++ Week-3/M07_Spring_REST/Ex05_Get_Country_By_Code/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,30 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CountryController.class);
+
+    private final CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        LOGGER.info("START");
+        Country country = countryService.getCountry(code);
+        LOGGER.info("END");
+        return country;
+    }
+}
